5a25481d9561e2c208523bbbf1238e3ec0170122
adw <alecwick@yahoo.com>
2020-12-06 00:08:05 -0800

    add alexnet-specific code

--- main.py
+++ main.py
@@ -19,25 +19,28 @@ from decompositions import cp_decomposition_conv_layer, tucker_decomposition_con
 # VGG16 based network for classifying between dogs and cats.
 # After training this will be an over parameterized network,
 # with potential to shrink it.
-class ModifiedVGG16Model(torch.nn.Module):
+class ModifiedAlexNetModel(torch.nn.Module):
     def __init__(self, model=None):
         super(ModifiedVGG16Model, self).__init__()
 
-        model = models.vgg16(pretrained=True)
+        model = models.alexnet(pretrained=True)
+
         self.features = model.features
 
         self.classifier = nn.Sequential(
             nn.Dropout(),
-            nn.Linear(25088, 4096),
+            nn.Linear(256 * 6 * 6, 4096),
             nn.ReLU(inplace=True),
             nn.Dropout(),
             nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
-            nn.Linear(4096, 2))
+            nn.Linear(4096, 6),
+        )
 
     def forward(self, x):
         x = self.features(x)
-        x = x.view(x.size(0), -1)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
         x = self.classifier(x)
         return x
 
