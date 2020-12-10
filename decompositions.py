a169748a50bf93b30e05837ce4f5ec9ae4707d3a
adw <alecwick@yahoo.com>
2020-12-10 01:14:41 -0800

    fix CP to use new version of tensorly

--- decompositions.py
+++ decompositions.py
@@ -9,44 +9,49 @@ def cp_decomposition_conv_layer(layer, rank):
     """ Gets a conv layer and a target rank, 
         returns a nn.Sequential object with the decomposition """
 
-    # Perform CP decomposition on the layer weight tensorly. 
-    last, first, vertical, horizontal = \
-        parafac(layer.weight.data, rank=rank, init='svd')
+print('rank chosen: {}'.format(rank))
+    # Perform CP decomposition on the layer weight tensorly.
+    
+    weights, factors = parafac(layer.weight.data, rank=rank, init='random')
+    t_dim, s_dim, y_dim, x_dim = factors
+    # last, first, vertical, horizontal = \
+    #     parafac(layer.weight.data, rank=rank, init='svd')
 
-    pointwise_s_to_r_layer = torch.nn.Conv2d(in_channels=first.shape[0], \
-            out_channels=first.shape[1], kernel_size=1, stride=1, padding=0, 
+    pointwise_s_to_r_layer = torch.nn.Conv2d(in_channels=s_dim.shape[0], \
+            out_channels=s_dim.shape[1], kernel_size=1, stride=1, padding=0, 
             dilation=layer.dilation, bias=False)
 
-    depthwise_vertical_layer = torch.nn.Conv2d(in_channels=vertical.shape[1], 
-            out_channels=vertical.shape[1], kernel_size=(vertical.shape[0], 1),
+    depthwise_vertical_layer = torch.nn.Conv2d(in_channels=y_dim.shape[1], 
+            out_channels=y_dim.shape[1], kernel_size=(y_dim.shape[0], 1),
             stride=1, padding=(layer.padding[0], 0), dilation=layer.dilation,
-            groups=vertical.shape[1], bias=False)
+            groups=y_dim.shape[1], bias=False)
 
     depthwise_horizontal_layer = \
-        torch.nn.Conv2d(in_channels=horizontal.shape[1], \
-            out_channels=horizontal.shape[1], 
-            kernel_size=(1, horizontal.shape[0]), stride=layer.stride,
+        torch.nn.Conv2d(in_channels=x_dim.shape[1], \
+            out_channels=x_dim.shape[1], 
+            kernel_size=(1, x_dim.shape[0]), stride=layer.stride,
             padding=(0, layer.padding[0]), 
-            dilation=layer.dilation, groups=horizontal.shape[1], bias=False)
+            dilation=layer.dilation, groups=x_dim.shape[1], bias=False)
 
-    pointwise_r_to_t_layer = torch.nn.Conv2d(in_channels=last.shape[1], \
-            out_channels=last.shape[0], kernel_size=1, stride=1,
+    pointwise_r_to_t_layer = torch.nn.Conv2d(in_channels=t_dim.shape[1], \
+            out_channels=t_dim.shape[0], kernel_size=1, stride=1,
             padding=0, dilation=layer.dilation, bias=True)
 
     pointwise_r_to_t_layer.bias.data = layer.bias.data
 
     depthwise_horizontal_layer.weight.data = \
-        torch.transpose(horizontal, 1, 0).unsqueeze(1).unsqueeze(1)
+        torch.transpose(x_dim, 1, 0).unsqueeze(1).unsqueeze(1)
     depthwise_vertical_layer.weight.data = \
-        torch.transpose(vertical, 1, 0).unsqueeze(1).unsqueeze(-1)
+        torch.transpose(y_dim, 1, 0).unsqueeze(1).unsqueeze(-1)
     pointwise_s_to_r_layer.weight.data = \
-        torch.transpose(first, 1, 0).unsqueeze(-1).unsqueeze(-1)
-    pointwise_r_to_t_layer.weight.data = last.unsqueeze(-1).unsqueeze(-1)
+        torch.transpose(s_dim, 1, 0).unsqueeze(-1).unsqueeze(-1)
+    pointwise_r_to_t_layer.weight.data = t_dim.unsqueeze(-1).unsqueeze(-1)
 
     new_layers = [pointwise_s_to_r_layer, depthwise_vertical_layer, \
                     depthwise_horizontal_layer, pointwise_r_to_t_layer]
     
     return nn.Sequential(*new_layers)
+
 
 def estimate_ranks(layer):
     """ Unfold the 2 modes of the Tensor the decomposition will 
